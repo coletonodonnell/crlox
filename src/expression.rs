572e4e2820136a33cb00612198b00fce4a9b0cac
coletonodonnell <contact@coleton.io>
2022-02-09 22:37:40 -0500

    Remove show and add Variable to Expression

--- src/expression.rs
+++ src/expression.rs
@@ -17,44 +17,8 @@ pub enum Expr {
         operator: Token,
         right: Box<Expr>
     },
-}
-
-impl Expr {
-    // Pretty Print an Expr
-    // TODO: Implement this show using visitor instead of this garbage.
-    pub fn show(expr: Expr) -> String {
-        // Match expr, as we require different steps to generate a string depending on the type
-        match expr {
-            Expr::Binary {left, operator, right} => {
-                return format!("({} {} {})", operator.lexeme, Expr::show(*left), Expr::show(*right));
-            },
-            Expr::Grouping {expression} => {
-                return format!("(group {})", Expr::show(*expression));
-            },
-            Expr::Unary {operator, right} => {
-                return format!("({} {})", operator.lexeme, Expr::show(*right));
-            }
-            Expr::Literal {value} => {
-                match value {
-                    Literal::Str(a) => {
-                        return a;
-                    },
-                    Literal::Num(a) => {
-                        return a.to_string();
-                    },
-                    Literal::False => {
-                        return "false".to_string();
-                    },
-                    Literal::True => {
-                        return "true".to_string();
-                    },
-                    Literal::Nill => {
-                        return "nill".to_string();
-                    }
-                }
-                
-            }
-        }
+    Variable {
+        token: Token
     }
 }
 
@@ -65,6 +29,7 @@ pub trait ExprVisitor<T> {
             Expr::Grouping {expression} => self.visit_grouping(expression),
             Expr::Literal {value} => self.visit_literal(value),
             Expr::Unary {operator, right} => self.visit_unary(operator, right),
+            Expr::Variable {token} => self.visit_variable(token),
         }
     }
 
@@ -72,4 +37,5 @@ pub trait ExprVisitor<T> {
     fn visit_grouping(&mut self, expression: Box<Expr>) -> Option<T>;
     fn visit_literal(&mut self, value: Literal) -> Option<T>;
     fn visit_unary(&mut self, operator: Token, right: Box<Expr>) -> Option<T>;
+    fn visit_variable(&mut self, token: Token) -> Option<T>;
 }
