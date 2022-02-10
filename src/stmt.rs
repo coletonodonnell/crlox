4cd8bd2f6bc0c17623bbba8bae08bcbc199e4f43
coletonodonnell <contact@coleton.io>
2022-02-09 22:37:29 -0500

    Add Var to Stmt

--- src/stmt.rs
+++ src/stmt.rs
@@ -1,4 +1,5 @@
 use crate::expression::Expr;
+use crate::token::Token;
 
 pub enum Stmt {
     Expression {
@@ -6,6 +7,10 @@ pub enum Stmt {
     },
     Print {
         expression: Expr
+    },
+    Var {
+        name: Token,
+        right: Option<Expr>,
     }
 }
 
@@ -13,10 +18,12 @@ pub trait StmtVisitor<T> {
     fn execute(&mut self, stmt: Stmt) -> T {
         match stmt {
             Stmt::Expression {expression: a} => self.visit_expression(a),
-            Stmt::Print {expression: a} => self.visit_print(a)
+            Stmt::Print {expression: a} => self.visit_print(a),
+            Stmt::Var {name: a, right: b} => self.visit_var(a, b)
         }
     }
 
     fn visit_expression(&mut self, expression: Expr) -> T;
     fn visit_print(&mut self, expression: Expr) -> T;
+    fn visit_var(&mut self, name: Token, right: Option<Expr>) -> T;
 }
