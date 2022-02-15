970de0f757a58833a6388f8005f8ab45d36584b1
coletonodonnell <contact@coleton.io>
2022-02-14 23:40:01 -0500

    Add error and assign in environment

--- src/environment.rs
+++ src/environment.rs
@@ -1,15 +1,23 @@
 use crate::token::{Literal, Token};
+use crate::expression::Expr;
 use std::collections::HashMap;
 
 pub struct Environment {
+    instance: crate::Lox,
     values: HashMap<String, Literal>
 }
 
 impl Environment {
-    pub fn build_envrionment() -> Environment {
+    pub fn build_envrionment(instance: crate::Lox) -> Environment {
         Environment {
+            instance: instance,
             values: HashMap::new()
         }
+    }
+
+    // If there is an error send it here to report to the Lox instance
+    fn error(&mut self, token: Token, message: String) {
+        self.instance.interpreter_error(token, &*message);
     }
 
     pub fn get(&mut self, name: Token) -> Result<Literal, String> {
@@ -22,5 +30,14 @@ impl Environment {
 
     pub fn define(&mut self, name: String, value: Literal) {
         self.values.insert(name, value);
+    }
+
+    pub fn assign(&mut self, name: Token, value: Literal) {
+        if self.values.contains_key(&name.lexeme) {
+            self.values.insert(name.lexeme, value);
+            return
+        }
+        
+        self.error(name.clone(), format!("Undefined variable {}.", name.lexeme));
     }
 }
